a02dc3ab6c353c69254d1feb86656399787d5c0d
zengzilin <zengzilin@users.noreply.github.com>
2024-10-01 23:41:11 +0800

    Update constants.ts

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -32,4 +32,11 @@ export const POSTS: { title: string; href: string; description: string }[] = [
     description:
       "A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.",
   },
+
+  {
+    title: "Book",
+    href: "/blog/book",
+    description:
+      "A popup that displays information related to an element when the summary of books.",
+  },
 ];
