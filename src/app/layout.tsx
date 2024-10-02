c7f638fca05ec90df34c2e4033c1614548096855
zengzilin2016@gmail.com <zengzilin2016@gmail.com>
2024-10-02 19:09:36 +0800

    添加google ads代码

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -62,7 +62,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head><Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7977474508095040" crossorigin="anonymous" /></head>
+      <head><Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7977474508095040" crossOrigin="anonymous" /></head>
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
