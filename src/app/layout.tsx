242bfc0586945d990ff875b99b3f3980f4a53587
zengzilin <zengzilin@users.noreply.github.com>
2024-10-08 10:16:49 +0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -62,7 +62,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head><Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7977474508095040" crossOrigin="anonymous" /></head>
+      <head><Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7977474508095040" crossOrigin="anonymous" /><GoogleTagManager gtmId="G-YKVZ7690ZX" /></head>
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
@@ -71,7 +71,6 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           <main className="flex-1">{children}</main>
-          <GoogleTagManager gtmId="G-YKVZ7690ZX" />
           <Footer />
         </ThemeProvider>
       </body>
